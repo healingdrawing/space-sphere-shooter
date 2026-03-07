24919564d16efb16ed3a735239a3e9a6bb307142
healingdrawing <healingdrawing@gmail.com>
2026-03-07 19:08:58 +0200

    random spawn without collision check. add big sphere into center to allow easier understand the positions, for dev at least.

--- vite/ts/src/views/game/xyz.ts
+++ vite/ts/src/views/game/xyz.ts
@@ -1,3 +1,6 @@
 export function xyz_dev(scene: BABYLON.Scene){
   new BABYLON.Debug.AxesViewer(scene, 15)
+
+  // add some market to now lost for dev at least
+  const gasphere = BABYLON.MeshBuilder.CreateSphere("big", {diameter:100},scene)
 }
