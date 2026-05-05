229820ceba45616cac6055a12f66bdf18775027e
healingdrawing <healingdrawing@gmail.com>
2026-05-05 11:00:22 +0300

    raw fix the lags with enemy ship rotations. The hole look like way in sync-orientation.ts . The rotation was ignored/cached, so second rotation was incorrect always.
    To avoid this, next projects consider:
    - set ccw rotation flag for babylonjs, from the beginning(this time code base was already affected by this mistake, i just found it later than needed)
    - consider quaternion usage(both server and client), instead of sequential rotation. Generally it is faster, but rodrigues rotation around axis formula can beat the quaternion in simple cases.
    
    The client is still absolutely not refactored for speed uses new 3d specific functions from gemm, but it is minor(not a server, with free tier, and limited everything).

--- vite/ts/src/views/game/sync-orientation.ts
+++ vite/ts/src/views/game/sync-orientation.ts
@@ -16,6 +16,8 @@ export function syncOrientation(ship_box: BABYLON.TransformNode, fvx: number, fv
   console.warn("BEFORE SYNC: server_top_v", server_top_v, "mesh_top_v", mesh_top_v, "fix_t_axis", fix_t_axis, "fix_t_angle", fix_t_angle)// todo remove
   if(fix_t_angle && gemm.vecXDnorm(raw_t_axis)) ship_box.rotateAround(mcv, fix_t_axis, fix_t_angle)
   
+  ship_box.computeWorldMatrix(true)
+
   const mesh_top_v2 = ship_box.getDirection(BABYLON.Vector3.Up()).asArray()
   console.warn("AFTER SYNC: server_top_v", server_top_v, "mesh_top_v", mesh_top_v2)// todo remove
   
@@ -28,4 +30,5 @@ export function syncOrientation(ship_box: BABYLON.TransformNode, fvx: number, fv
   console.log("fix_f_axis", fix_f_axis, "fix_f_angle", fix_f_angle)
   if(fix_f_angle && gemm.vecXDnorm(raw_f_axis)) ship_box.rotateAround(mcv, fix_f_axis, fix_f_angle)
   
+  // todo // warning at the moment ship_box.computeWorldMatrix(true) visually fix the lags with rotations of the enemy ships. Tested shallow/visually. Possibly need one more call after second rotation (code line above).
 }
