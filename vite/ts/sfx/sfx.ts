a2f13b05adcc9e385b5352b7a8e1881608cf3157
healingdrawing <healingdrawing@gmail.com>
2026-05-14 00:02:55 +0300

    sfx moved to public folder. for deploy

--- vite/ts/sfx/sfx.ts
+++ vite/ts/sfx/sfx.ts
@@ -4,10 +4,10 @@ class SFXBox {
   constructor() {
     (async () => {
       const audioEngine = await BABYLON.CreateAudioEngineAsync();
-      this.sounds.hit = await BABYLON.CreateSoundAsync("hit", "./sfx/hit.wav");
-      this.sounds.move = await BABYLON.CreateSoundAsync("move", "./sfx/move.wav");
-      this.sounds.rotate = await BABYLON.CreateSoundAsync("rotate", "./sfx/rotate.wav");
-      this.sounds.shot = await BABYLON.CreateSoundAsync("shot", "./sfx/shot.wav");
+      this.sounds.hit = await BABYLON.CreateSoundAsync("hit", "/hit.wav");
+      this.sounds.move = await BABYLON.CreateSoundAsync("move", "/move.wav");
+      this.sounds.rotate = await BABYLON.CreateSoundAsync("rotate", "/rotate.wav");
+      this.sounds.shot = await BABYLON.CreateSoundAsync("shot", "/shot.wav");
       // Wait for the audio engine to unlock
       await audioEngine.unlockAsync();
       // this.sounds.shot.play()
